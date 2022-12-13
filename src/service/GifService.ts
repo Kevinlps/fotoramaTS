6baea36525664f0d3aa33e5b48571d7fd323d068
Kevin <you@example.com>
2022-12-13 13:48:50 -0400

    .

--- src/service/GifService.ts
+++ src/service/GifService.ts
@@ -1,8 +1,6 @@
 import { GifsDTO } from '../data-transports/GifsDTO'
 import { GifClass } from '../models/GifClass'
-import { GiphyFetch } from '@giphy/js-fetch-api'
-import { Gif } from '@giphy/react-components'
-import { urlToHttpOptions } from 'url'
+import { GiphyFetch, PaginationOptions } from '@giphy/js-fetch-api'
 
 const Gifapi = new GiphyFetch(import.meta.env.VITE_UNSPLASH_ACCESS_KEY) 
 
@@ -11,7 +9,8 @@ export class GifService {
   async findGifs(
     query: string,
     page: number,
-    perPage: number
+    perPage: number,
+    
   ): Promise<GifsDTO> {
     let gifsDTO: GifsDTO = {
       gifs: [],
@@ -19,16 +18,13 @@ export class GifService {
     }
 
     if (query) {
+     
       const result = await Gifapi.search(query)
-      
-      console.log(result)
-      
+
       const { data , pagination } = result
       if (data) {
-        page = pagination.count 
-        perPage= pagination.count
-
         const gifs = data.map((r) => new GifClass(r) )
+        
         console.log(result.data)
         gifsDTO = {
           gifs,
